ba1176f35faf32eff0e44f723758875bb2a6f3c3
Euclides Rodrigues Junior <eur.juninho@gmail.com>
2021-01-28 15:05:42 -0300

    atualização do curso de js

--- Udemy/curse_of_javascript/06-Modal/script.js
+++ Udemy/curse_of_javascript/06-Modal/script.js
@@ -8,17 +8,24 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 // console.log(btnsOpenModal);
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', function() {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-
-    const closeModal = function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    };
-
-    btnCloseModal.addEventListener('click', closeModal);
-    overlay.addEventListener('click', closeModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
