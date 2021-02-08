af67b8b0b6b404a0947029b798da54758cedd504
Euclides Rodrigues Junior <eur.juninho@gmail.com>
2021-02-08 16:16:48 -0300

    First commit 🚀

---
+++ Udemy/curse_of_javascript/09-Data-Structures-Operators/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+/*
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+let [main, , primary] = restaurant.starterMenu;
+// console.log(main, primary);
+
+// Receive 2 return from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+*/
