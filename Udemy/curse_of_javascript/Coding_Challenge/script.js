e8780b740a2483f6093ccc32a4d76ff5cc0a6fd3
Euclides-R <euclides.rodriguesjr@outlook.com>
2021-04-12 13:08:46 -0300

    atualization course online JS 💻

--- Udemy/curse_of_javascript/Coding_Challenge/script.js
+++ Udemy/curse_of_javascript/Coding_Challenge/script.js
@@ -180,3 +180,5 @@ console.log(dogs.filter(checkEatingOkay));
 // sort it by recommended food portion in an ascending order [1,2,3]
 const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
 console.log(dogsSorted);
+
+window.alert(dogsSorted);
